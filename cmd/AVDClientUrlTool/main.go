b8413b57bd48843e29b17f959c7213a6ed0fb097
jbinko <jbinko+github@gmail.com>
2022-07-16 23:18:43 +0200

    checkDnsRecords

--- cmd/AVDClientUrlTool/main.go
+++ cmd/AVDClientUrlTool/main.go
@@ -2,9 +2,50 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
+type dnsRecord struct {
+	name  string
+	ips   []string
+	error error
+}
+
+func checkDnsRecords(dnsRecords []dnsRecord) {
+
+	for _, dnsRecord := range dnsRecords {
+		checkDnsRecord(dnsRecord)
+	}
+}
+
+func checkDnsRecord(dnsRecord dnsRecord) {
+
+	ips, err := net.LookupIP(dnsRecord.name)
+	if err != nil {
+		dnsRecord.error = err
+	} else {
+
+		ipsString := make([]string, len(ips))
+
+		for i := 0; i < len(ips); i++ {
+			ipsString[i] = ips[i].String()
+		}
+	}
+}
+
 func main() {
+
+	dnsRecords := []dnsRecord{
+		{
+			name: "client.wvd.microsoft.com",
+		},
+		{
+			name: "client2.wvd.microsoft.com",
+		},
+	}
+
+	checkDnsRecords(dnsRecords)
+
 	fmt.Println("\n")
 	fmt.Println("AVD")
 	fmt.Println("======================================================")
