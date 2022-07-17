1fed886f7c0c2e97d3328c74e60c1502e278549b
jbinko <jbinko+github@gmail.com>
2022-07-17 11:54:14 +0200

    printDnsRecords

--- cmd/AVDClientUrlTool/main.go
+++ cmd/AVDClientUrlTool/main.go
@@ -21,14 +21,14 @@ func checkDnsRecords(dnsRecords []dnsRecord) {
 	for i := 0; i < dnsRecordsCount; i++ {
 		go func(i int) {
 			defer wg.Done()
-			checkDnsRecord(dnsRecords[i])
+			checkDnsRecord(&dnsRecords[i])
 		}(i)
 	}
 
 	wg.Wait()
 }
 
-func checkDnsRecord(dnsRecord dnsRecord) {
+func checkDnsRecord(dnsRecord *dnsRecord) {
 
 	ips, err := net.LookupIP(dnsRecord.name)
 	if err != nil {
@@ -41,31 +41,70 @@ func checkDnsRecord(dnsRecord dnsRecord) {
 		for i := 0; i < ipsCount; i++ {
 			ipsString[i] = ips[i].String()
 		}
+
+		dnsRecord.ips = ipsString
+	}
+}
+
+func printDnsRecords(dnsRecords []dnsRecord, printFailed bool) {
+
+	for _, dnsRecord := range dnsRecords {
+
+		if printFailed == true {
+
+			if dnsRecord.error != nil {
+				fmt.Printf("%s (%s)\n", dnsRecord.name, dnsRecord.error)
+			}
+		} else {
+
+			if dnsRecord.error == nil {
+				for _, ip := range dnsRecord.ips {
+					fmt.Printf("%s (%s)\n", dnsRecord.name, ip)
+				}
+			}
+		}
 	}
 }
 
 func main() {
 
+	// https://docs.microsoft.com/en-us/azure/virtual-desktop/safe-url-list?tabs=azure#remote-desktop-clients
 	dnsRecords := []dnsRecord{
 		{
 			name: "client.wvd.microsoft.com",
 		},
 		{
-			name: "client2.wvd.microsoft.com",
+			name: "watchdog.servicebus.windows.net",
+		},
+		{
+			name: "go.microsoft.com",
+		},
+		{
+			name: "aka.ms",
+		},
+		{
+			name: "docs.microsoft.com",
+		},
+		{
+			name: "privacy.microsoft.com",
+		},
+		{
+			name: "query.prod.cms.rt.microsoft.com",
 		},
 	}
 
 	checkDnsRecords(dnsRecords)
 
 	fmt.Println("\n")
-	fmt.Println("AVD")
+	fmt.Println("AVD Client Connectivity Check Tool")
 	fmt.Println("======================================================")
 	fmt.Println("\n")
 	fmt.Println("NOT Resolvable URLs:")
 	fmt.Println("======================================================")
+	printDnsRecords(dnsRecords, true)
 	fmt.Println("\n")
 	fmt.Println("Resolvable URLs:")
 	fmt.Println("======================================================")
-	fmt.Println("client.wvd.microsoft.com")
+	printDnsRecords(dnsRecords, false)
 	fmt.Println("\n")
 }
